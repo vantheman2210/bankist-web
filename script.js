afe9a3075588469819892758d2d6a1dc0609ba1b
vantheman2210 <martinovic.ba@gmail.com>
2023-04-29 12:29:52 +0200

    Adding lazy loading images

--- script.js
+++ script.js
@@ -177,7 +177,6 @@ headerObserver.observe(header);
 const allSections = document.querySelectorAll('.section');
 const revealSection = function(entries, observer) {
 	const [ entry ] = entries;
-	console.log(entry);
 	if (!entry.isIntersecting) return;
 	entry.target.classList.remove('section--hidden');
 	observer.unobserve(entry.target);
@@ -192,3 +191,23 @@ allSections.forEach(function(section) {
 	sectionObserver.observe(section);
 	section.classList.add('section--hidden');
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries, observer) { 
+	const [entry] = entries;
+
+	if (!entry.isIntersecting) return;
+
+	// Replace src attribute with data-src
+	entry.target.src = entry.target.dataset.src
+	entry.target.style.filter = 'blur(0px)';
+}
+
+const imgObserver = new IntersectionObserver(loadImg, { 
+	root: null,
+	threshold: 0
+});
+
+imgTargets.forEach(img => imgObserver.observe(img))
